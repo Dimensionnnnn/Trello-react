fbc23b84efb94eadc7150fc57b3e7c5ce2a2a101
dimensionnn <t.golovin55@gmail.com>
2023-10-02 14:24:50 +0600

    fix/hot fix, deleted not using code

--- src/components/popup-card/popup-card.tsx
+++ src/components/popup-card/popup-card.tsx
@@ -1,6 +1,6 @@
 import { Popup } from "components/UI/popup/popup"
-import { useFocusAndSelect } from "hooks/useFocusAndSelect";
-import { useRef, useState } from "react";
+import { } from "hooks/useFocusAndSelect";
+import { useState } from "react";
 import { Card } from "types/types";
 import { Button } from "components/UI/button/button";
 import { EditableText } from "components/editable-text/editable-text";
@@ -16,21 +16,6 @@ interface Props {
 export const PopupCard: React.FC<Props> = ({isOpen, onClose, card, onCardTextChange, onDescriptionChange}) => {
     const [activeCardTitleIdEditing, setActiveCardTitleIdEditing] = useState<string | undefined>();
     const [activeCardDescriptionIdEditing, setActiveCardDescriptionIdEditing] = useState<string | undefined>();
-
-    const textAreaTitleRef = useRef<HTMLTextAreaElement>(null);
-    const textAreaDescriptionRef = useRef<HTMLTextAreaElement>(null);
-
-    useFocusAndSelect({
-        ref: textAreaTitleRef,
-        condition: activeCardTitleIdEditing === card?.id,
-        value: card?.title,
-    });
-
-    useFocusAndSelect({
-        ref: textAreaDescriptionRef,
-        condition: activeCardDescriptionIdEditing === card?.id,
-        value: card?.description,
-    })
 
     return (
         <Popup
